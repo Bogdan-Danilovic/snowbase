8be5d3bccce3b61e635c1a7dd8926fd9519f9ebf
Bogdan Danilovic <sekirabd@gmail.com>
2026-05-25 17:05:40 +0200

    switch to PRAVA version

--- debug.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "MYSQLHOST: " . getenv('MYSQLHOST') . "<br>";
-echo "MYSQLPORT: " . getenv('MYSQLPORT') . "<br>";
-echo "MYSQLDATABASE: " . getenv('MYSQLDATABASE') . "<br>";
-echo "MYSQLUSER: " . getenv('MYSQLUSER') . "<br>";
