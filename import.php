002d1f561ffe9e4f977223f6b02ba177606fb98d
Bogdan Danilovic <sekirabd@gmail.com>
2026-05-25 16:44:30 +0200

    remove import files

--- import.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/db.php';
-$sql = file_get_contents(__DIR__ . '/snowbase_dump.sql');
-$pdo->exec($sql);
-echo "Import OK - obrisi import.php i snowbase_dump.sql sada!";
