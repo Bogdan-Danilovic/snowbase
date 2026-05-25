b4cce45d2687f31721742339d42ba693f0c71c20
Bogdan Danilovic <sekirabd@gmail.com>
2026-05-25 16:04:08 +0200

    debug env vars

---
+++ debug.php
@@ -0,0 +1,5 @@
+<?php
+echo "MYSQLHOST: " . getenv('MYSQLHOST') . "<br>";
+echo "MYSQLPORT: " . getenv('MYSQLPORT') . "<br>";
+echo "MYSQLDATABASE: " . getenv('MYSQLDATABASE') . "<br>";
+echo "MYSQLUSER: " . getenv('MYSQLUSER') . "<br>";
